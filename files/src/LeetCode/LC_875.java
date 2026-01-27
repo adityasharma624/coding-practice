8908bd31943ab2b6880a733071bf3074064cbb33
Aditya Sharma <adityas.cse@gmail.com>
2026-01-27 14:12:05 +0530

    update

---
+++ files/src/LeetCode/LC_875.java
@@ -0,0 +1,20 @@
+/*
+875. Koko Eating Bananas
+Medium
+
+00:06 read the problem
+00:07 maybe we approach this as an infinite array problem, when we find an upper bound which is a possible number we do a binary search there
+00:07 this will cause us to do the calculation again and again.
+00:07 abandoning the problem
+
+ */
+
+package LeetCode;
+
+public class LC_875 {
+    static void main() {
+        int[] piles = {30,11,23,4,20};
+        int h = 5;
+
+    }
+}
