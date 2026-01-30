7c5a6516210783da27322bed1c64dd30c5925cbe
Aditya Sharma <adityas.cse@gmail.com>
2026-01-30 23:28:46 +0530

    LC_287 updated

--- files/src/LeetCode/LC_287.java
+++ files/src/LeetCode/LC_287.java
@@ -1,4 +1,41 @@
+/*
+287. Find the Duplicate Number
+Medium
+
+00:01 looking at the question, we can go with cyclic sort.
+00:03 i need to practice cyclic sort a bit more.
+00:04 accepted.
+ */
+
 package LeetCode;
 
 public class LC_287 {
+    static void main() {
+        int[] nums = {1,3,4,2,2};
+        System.out.println(findDuplicate(nums));
+    }
+
+    public static int findDuplicate(int[] nums) {
+        int i = 0;
+        while (i < nums.length)
+        {
+            if (nums[i] != nums[nums[i]-1])
+            {
+                int correct = nums[i] - 1;
+                int temp = nums[correct];
+                nums[correct] = nums[i];
+                nums[i] = temp;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1)
+                return nums[j];
+        }
+        return -1;
+    }
 }
