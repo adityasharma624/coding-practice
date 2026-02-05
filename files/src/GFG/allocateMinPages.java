8cc11baf4e7787c0b735d69918f5e6ec3f73513b
Aditya Sharma <adityas.cse@gmail.com>
2026-02-05 19:05:26 +0530

    updated allocateMinPages problem

--- files/src/GFG/allocateMinPages.java
+++ files/src/GFG/allocateMinPages.java
@@ -20,19 +20,20 @@ public class allocateMinPages {
         if (arr.length < k)
             return -1;
 
-        int end = 0;
-        int start = arr[0];
-        for (int page : arr) {;
-            end += page;
-            start = Math.max(start, page);
+        int start = 0, end = 0;
+        for (int book : arr)
+        {
+            end += book;
+            start = Math.max(start, book);
         }
 
-        int ans = 0;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (isPage(arr, k, mid))
+        int cand = -1;
+        while (start <= end)
+        {
+            int mid = start + (end - start)/2;
+            if (check(arr, k, mid))
             {
-                ans = mid;
+                cand = mid;
                 end = mid - 1;
             }
             else
@@ -40,27 +41,26 @@ public class allocateMinPages {
                 start = mid + 1;
             }
         }
-        return ans;
+        return cand;
     }
 
-    static boolean isPage(int[] arr, int k, int mid)
+    static boolean check(int[] arr, int k, int threshold)
     {
-        int students = 1;
+        int count = 1;
         int sum = 0;
-        for (int page : arr)
+        for (int book : arr)
         {
-            if (sum + page > mid)
+            if (sum + book > threshold)
             {
-                students++;
-                sum = page;
+                count++;
+                sum = book;
             }
-
-            if (students > k)
-                return false;
             else
-                sum += page;
+            {
+                sum += book;
+            }
         }
 
-        return true;
+        return count <= k;
     }
 }
